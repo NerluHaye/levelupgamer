967ad9f437da6b0a00424edd10c5a35832e696ac
nicol <ni.jerezm@duocuc.cl>
2025-11-15 20:29:16 -0300

    Fix : Typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,5 +87,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     // Corrutinas para trabajo asincronico
-    implementation("org.jetbrains.kotlin:kotlin-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+
 }
