b05884dfeb87e9cbe114eee36de488326ab0bc93
Abraham2143 <ab.neira@duocuc.cl>
2025-11-17 01:51:24 -0300

    fix: implementacion open

--- app/src/main/java/com/example/levelupgamer/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/levelupgamer/data/repository/AuthRepository.kt
@@ -4,13 +4,13 @@ import com.example.levelupgamer.data.remote.ApiService
 import com.example.levelupgamer.data.remote.model.LoginDTO
 import com.example.levelupgamer.data.remote.model.RegistroUsuarioDTO
 import com.example.levelupgamer.data.remote.model.UsuarioDTO
-class AuthRepository(private val apiService: ApiService) {
+open class AuthRepository(private val apiService: ApiService) {
 
-    suspend fun login(loginDTO: LoginDTO): UsuarioDTO {
+    open suspend fun login(loginDTO: LoginDTO): UsuarioDTO {
         return apiService.loginUsuario(loginDTO)
     }
 
-    suspend fun register(registroUsuarioDTO: RegistroUsuarioDTO): UsuarioDTO {
+    open suspend fun register(registroUsuarioDTO: RegistroUsuarioDTO): UsuarioDTO {
         return apiService.registrarUsuario(registroUsuarioDTO)
     }
 
